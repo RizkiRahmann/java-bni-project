31c93205a0dd56ea53c20c64c729e35edb948a83
RizkiRahmann <riskirahman1116@gmail.com>
2025-06-10 10:46:31 +0700

    TEST BARU

---
+++ src/main/java/com/bni/bni/controller/FileController.java
@@ -0,0 +1,79 @@
+package com.bni.bni.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+// import java.net.MalformedURLException;
+import java.nio.file.*;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/files")
+public class FileController {
+    @Value("${file.upload-dir}")
+    private String uploadDir;
+
+    @PostMapping("/upload")
+    public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile file) {
+        try{
+            Path uploadPath = Paths.get(uploadDir);
+            if (!Files.exists(uploadPath)) {
+                Files.createDirectories(uploadPath);
+            }
+
+            String fileName = file.getOriginalFilename();
+            Path filePath = uploadPath.resolve(fileName);
+            Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+
+            String fileUrl = "/api/files/" + fileName;
+
+            return ResponseEntity.ok().body(
+                Map.of(
+                    "status", 200,
+                    "message", "File uploaded successfully",
+                    "fileUrl", fileUrl
+                )
+            );
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(
+                Map.of(
+                    "status", 500,
+                    "message", "File upload failed: " + e.getMessage()
+                )
+            );
+        }
+    }
+
+    @GetMapping("/{fileName:.+}")
+    public ResponseEntity<Resource> getFile(@PathVariable String fileName) {
+        try {
+            Path filePath = Paths.get(uploadDir).resolve(fileName).normalize();
+            Resource resource = new UrlResource(filePath.toUri());
+
+            if (!resource.exists()) {
+                return ResponseEntity.notFound().build();  
+            }
+
+            String contentType = Files.probeContentType(filePath);
+            if (contentType == null) {
+                contentType = "application/octet-stream"; // Default content type
+            }
+
+            return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "Inline; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
+
+        } catch (Exception e) {
+            return ResponseEntity.status(500).build();
+        }
+    }
+}
