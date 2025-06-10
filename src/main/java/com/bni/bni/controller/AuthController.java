bafa59211566e9ab4660950222e3f7d988c953f4
RizkiRahmann <riskirahman1116@gmail.com>
2025-06-10 21:41:36 +0700

    DAH BISA NICH......3

--- src/main/java/com/bni/bni/controller/AuthController.java
+++ src/main/java/com/bni/bni/controller/AuthController.java
@@ -63,7 +63,7 @@ public class AuthController {
             response.put("token", token);
             response.put("username", user.getUsername());
             response.put("role", user.getRole());
-            response.put("message", "Login Berhasil");
+            response.put("message", "Login Berhasil Sekali");
             return ResponseEntity.ok(response);
         } else {
             response.put("status", 401);
