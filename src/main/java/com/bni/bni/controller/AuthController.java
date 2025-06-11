f28d8095b85c5eed4f62595166c89878da52604b
RizkiRahmann <riskirahman1116@gmail.com>
2025-06-11 07:55:12 +0700

    OKELAH YAH

--- src/main/java/com/bni/bni/controller/AuthController.java
+++ src/main/java/com/bni/bni/controller/AuthController.java
@@ -63,7 +63,7 @@ public class AuthController {
             response.put("token", token);
             response.put("username", user.getUsername());
             response.put("role", user.getRole());
-            response.put("message", "Login Bisa Okedeh");
+            response.put("message", "Login Bisa Hore");
             return ResponseEntity.ok(response);
         } else {
             response.put("status", 401);
